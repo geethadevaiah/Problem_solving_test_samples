1f75191b53404e61c161ba4b19eb8997cd85b4ae
geethadevaiah <geetha.d19@gmail.com>
2020-08-24 16:36:09 +0530

    Update removeNthNodeFromTheEndLL.java

--- removeNthNodeFromTheEndLL.java
+++ removeNthNodeFromTheEndLL.java
@@ -2,7 +2,18 @@
 public class removeNthNodeFromTheEndLL {
 
 	public static ListNode removeNthFromEnd(ListNode head, int n) {
-        
+		ListNode tmpHead = head;
+        ListNode curr = head;
+        ListNode prev = head;
+        while(n >= 0 && curr != null && curr.next != null) {
+        	prev = curr;
+        	curr = curr.next;
+        	n--;
+        }
+        if(curr.next == null)
+            prev.next = null;
+        prev.next = curr.next;
+        return tmpHead;
     }
 	public static void main(String[] args) {
 		
