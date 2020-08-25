b73e80044f6426e94854c40667d4bc0e701cb95e
geethadevaiah <67411309+geethadevaiah@users.noreply.github.com>
2020-08-25 23:29:10 +0530

    Update FlattenTheNodes.java

--- FlattenTheNodes.java
+++ FlattenTheNodes.java
@@ -1,12 +1,41 @@
 
 public class FlattenTheNodes {
+	
+	public Node getLastNode(Node head) {
+		if(head == null) {
+			return null;
+		}
+		if(head.next == null) {
+			return head;
+		}
+		return getLastNode(head.next);
+	}
 
 	public Node flatten(Node head) {
 		if(head == null)
 			 return null;
-		//if(head.next == null)
-			//return head; // what if there child nodes but no next nodes
-		
+		// check for child nodes
+		// add them and then traverse till end for next node
+		Node curr = head;
+		while(curr != null) {
+			if(curr.child != null) {
+				Node next = curr.next;
+				Node child = flatten(curr.child); 
+				
+				curr.child = null;
+				curr.next = child;
+				child.prev = curr;
+				
+				while(curr.next != null)
+					curr = curr.next;
+				curr.next = next;
+				
+				if(next != null)
+					next.prev = curr;
+			}
+			curr = curr.next;
+		}
+		return head;
 	}
 	
 
