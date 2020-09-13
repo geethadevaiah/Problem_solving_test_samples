39da03707b8e09d2fbebd5a95f587f31c5140a53
geethadevaiah <67411309+geethadevaiah@users.noreply.github.com>
2020-09-13 23:13:25 +0530

    13Sept - Others

---
+++ Ohers/Rotate2DArrayClockwise.java
@@ -0,0 +1,15 @@
+package Ohers;
+
+public class Rotate2DArrayClockwise {
+
+	/**
+	 * Input: matrix = [[1,2],[3,4]]
+		Output: [[3,1],[4,2]]
+	 */
+	public void rotate(int[][] matrix) {
+		// 1. fold the matrix horizontally
+		// 2. fold the matrix diagonally
+		
+		
+	}
+}
