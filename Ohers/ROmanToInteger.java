7cb62bcbab86276fa173ddb0b966aa997ef38e2a
geethadevaiah <geetha.d19@gmail.com>
2020-09-19 09:27:48 +0530

    Update ROmanToInteger.java

--- Ohers/ROmanToInteger.java
+++ Ohers/ROmanToInteger.java
@@ -17,27 +17,34 @@ public class ROmanToInteger {
 	}
 	    
     public static int romanToInt(String s) {
-        if (s == "") return 0;
+//    	if (s == "") return 0;
         initializeMap();
         int num = 0;
+        if(s.length()<2)
+            return map.get(s.charAt(0));
         for(int i = 0 ; i < s.length() ; i++) {
-        	if((s.charAt(i) == 'I' || s.charAt(i) == 'X' || s.charAt(i) == 'C') &&  (i+1 < s.length())) {
+        	
+        	if(i+1 < s.length() && (map.get(s.charAt(i+1)) > map.get(s.charAt(i)) )) {
+        		num += map.get(s.charAt(i+1)) - (1 * map.get(s.charAt(i)));
+    			i++;
+        	}
+        	else num += map.get(s.charAt(i));
+        	// just check if the i+1 val is greater than i's val
+        	/*if((s.charAt(i) == 'I' || s.charAt(i) == 'X' || s.charAt(i) == 'C') &&  (i+1 < s.length())) {
         		if(s.charAt(i) == 'I' && (s.charAt(i+1) == 'V' || s.charAt(i+1)=='X') ||
         		(s.charAt(i) == 'X' && (s.charAt(i+1) == 'L'||s.charAt(i+1)=='C')) ||
         		(s.charAt(i) == 'C' && (s.charAt(i+1) == 'D' || s.charAt(i+1) == 'M'))){
         			num += map.get(s.charAt(i+1)) - (1 * map.get(s.charAt(i)));
         			i++;
         		}
-        		else 
-        			num += map.get(s.charAt(i));
+        		else num += map.get(s.charAt(i));
         	}
-        	else {
-        		num += map.get(s.charAt(i)) ;
-        	}
+        	else num += map.get(s.charAt(i)) ;
+        	*/
         }
         return num;
     }
      public static void main(String[] args) {
-		System.out.println(romanToInt("MC"));
+		System.out.println(romanToInt("MCMXCIV"));
 	}
 }
