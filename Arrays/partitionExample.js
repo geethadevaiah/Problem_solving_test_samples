ffbdb527f6105fb8e6d6d7c800471a96e5bdf57c
geethadevaiah <geetha.d19@gmail.com>
2021-02-27 14:05:42 +0530

    Create partitionExample.js

---
+++ Arrays/partitionExample.js
@@ -0,0 +1,22 @@
+var partitionlabels =  function(S) {
+    let lastIndex = {}
+
+    for (let i = 0 ; i < S.length ; i++){
+        lastIndex[S[i]] = i
+    }
+
+    let partitions = []
+    let start = 0
+    let end = 0
+
+    for(let i = 0 ; i < S.length ; i++){
+        end = Math.max(end, lastIndex[S[i]])
+        if( i == end ){
+            partitions.push(end - start + 1)
+            start = i + 1
+        }
+    }
+    return partitions
+}
+partitions = partitionlabels("ababcbacadefegdehijhklij");
+console.info(partitions)
