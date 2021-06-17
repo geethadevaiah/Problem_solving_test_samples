1145c4594019220ce399061e441b5c4dcd1b0f67
geethadevaiah <67411309+geethadevaiah@users.noreply.github.com>
2021-06-17 14:54:28 +0530

    Update MergeSortImpl.java

--- Sorting/MergeSortImpl.java
+++ Sorting/MergeSortImpl.java
@@ -5,33 +5,54 @@ public class MergeSortImpl {
 	
 	public static void mergeSortImpl(int[] a) {
 		
-		
+		divide(a, 0 , a.length);
+		merge(a, 0 , a.length);
 	}
 	
-	public void divide(int[] a, int l , int r) {
-		
-		if(r-l >= 0) {
-			int mid = r - (l + r) / 2;
+	public static void divide(int[] a, int l , int r) {
+		//r-l >= 0
+		if(l < r) {
+			System.out.println("inside divide function!");
+			int mid = l + (r - l) / 2;
 			divide(a, l, mid);
-			
+			merge(a, 0 , a.length);
 			divide(a, r, mid+1);
+			
+			
 		}
 	}
 	
-	public void merge(int[] a , int l, int r) {
+	public static void merge(int[] a , int l, int r) {
+		
+		System.out.println("in merge function!");
 		
 		int n = a.length;
-		int mid = r - (l + r) / 2;
+		int mid = l + (r - l) / 2;
 		int i = 0, j = mid+1;
-		while(i < mid || j < n) {
-			
+		while((i < n & i < mid) || j < n) {
+			if(a[i] <= a[j]) {
+				i++;
+			}
+			else if(a[i] > a[j]) {
+				// number in the second part is greater
+				// push all the elements by right 1
+				int k = j;
+				int key = a[k];
+				while(k > i) {
+					a[k] = a[k-1];
+					k--;
+				}
+				j++;
+				a[i] = key;
+			}
 		}
+		System.out.println("Check the array now to be sorted!");
 	}
 	
 	
 	public static void main(String[] args) {
-		int[] arr = {3, 4, 5, 1, 6 , 3, 0, 4};
-		mergeSortFunc(arr);
+		int[] arr = {3, 4, 5, 1, 6, 0};
+		divide(arr, 0 , arr.length);
 		for(int i = 0 ; i < arr.length ; i++)
 		System.out.println(arr[i]);
 	}
