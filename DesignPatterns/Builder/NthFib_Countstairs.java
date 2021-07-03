30ec489427469c3e31f8d70a4cfa0f09a70ae9cc
geethadevaiah <67411309+geethadevaiah@users.noreply.github.com>
2021-07-03 09:00:56 +0530

    Update NthFib_Countstairs.java

--- DesignPatterns/Builder/NthFib_Countstairs.java
+++ DesignPatterns/Builder/NthFib_Countstairs.java
@@ -19,3 +19,17 @@ public class NthFib_Countstairs {
 		System.out.println(no_waysToReachNStair(4));
 	}
 }
+/**
+*	Find a way to climb the n-th stairs when one can take either one stp or two steps at one time
+*   
+*	At 1st step, there is only one option to climb = 1
+*   At second step, 2 options -> one step twice or two steps at once.
+*
+*   So at every step there is option to choose either previous option or last-to-second previous option
+*   ways(n) = ways(n-1) + ways(n-2)
+*   
+*   this is nothing but fib ( n + 1 )
+*   
+*   Finding the n-th fibonacci number
+*
+***/
