dc1474adff9b9160e6a65b26162fc7e849754d6f
geethadevaiah <67411309+geethadevaiah@users.noreply.github.com>
2021-07-06 22:28:32 +0530

    Update PrintAllSubsets.java

--- Class_revision/Recursion_and_Backtracking/PrintAllSubsets.java
+++ Class_revision/Recursion_and_Backtracking/PrintAllSubsets.java
@@ -1,5 +1,8 @@
 package Class_revision.Recursion_and_Backtracking;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PrintAllSubsets {
 
 	
@@ -20,11 +23,36 @@ public class PrintAllSubsets {
 		printSubset(arr, i+1, osf);
 	}
 	
+	// Using backtracking
+	public static void printSubsetsBacktracking(int[] arr, List<Integer> temp, int i, int n) {
+		
+		// Base case, print the subset list when the size is full
+		if(i == n) {
+			if(temp.size() > 0) {
+				for(int j = 0 ; j < temp.size() ; j++) {
+					System.out.print(temp.get(j));
+					return;
+				}
+				System.out.println();
+			}
+		}
+		
+		// recursion
+		temp.add(arr[i]);
+		printSubsetsBacktracking(arr, temp, i+1, n);
+		System.out.println(temp.size());
+		temp.remove(temp.size()-1);
+		printSubsetsBacktracking(arr, temp, i+1, n);
+		
+	}
+	
 	
 	public static void main(String[] args) {
 		
 		int arr[] = {1, 2, 3};
-		printSubset(arr, 0, "");
+//		printSubset(arr, 0, "");
+		List<Integer> list = new ArrayList<>();
+		printSubsetsBacktracking(arr, list, 0, arr.length);
 	}
 	
 	
