bc681081ce5db8d9408120da4fa02f639f9b3560
geethadevaiah <67411309+geethadevaiah@users.noreply.github.com>
2021-08-09 13:08:35 +0530

    Update ThreeSum.java

--- Arrays/ThreeSum.java
+++ Arrays/ThreeSum.java
@@ -40,12 +40,44 @@ public class ThreeSum {
 		// send the array list back
 		return new ArrayList<List<Integer>>(result);
 	}
+	
+	
+	public static List<List<Integer>> getThreeTripletsSum(int[] num){
+		
+		List<List<Integer>> result = new ArrayList<>();
+		
+		// two pointer technique (with fixing one pointer to get the rest two pointer traversing
+		// Sort the array
+		Arrays.sort(num);
+		for(int i = 0 ; i < num.length ; i++) {
+			// skip duplicates
+			if(i != 0 && num[i] == num[i-1]) continue;
+			// two pointers, pointing second and last index
+			int j = i+1, k = num.length - 1;
+			
+			while(j < k) {
+				if(num[i] + num[j] + num[k] == 0) { // found the triplet
+					result.add(Arrays.asList(num[i] , num[j], num[k]));
+					j++;
+					// skip the duplicates
+					while(j < k && num[j] == num[j-1]) j++;
+				}
+				// move the j pointer if the value is less than 0
+				else if ( num[i] + num[j] + num[k] < 0) j++;
+				// move the k pointer left, if the value sum is greater
+				else k--;
+			}
+		}
+		
+		return result;
+	}
 
 	public static void main(String[] args) {
 //		int[] nums = {-1, 0 , 1, 2 , -1, -4};
-		int[] nums = {-2,0,0,2,2};
+		int[] nums = {-2,0,0,2,2, 1, 0 , -1};
 
 		System.out.println(calculateThreeSum(nums));
+		System.out.println(getThreeTripletsSum(nums));
 			
 	}
 
