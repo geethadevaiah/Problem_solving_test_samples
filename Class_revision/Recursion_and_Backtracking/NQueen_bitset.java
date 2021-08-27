41f0bde293382f9fae6c9bfc9f0a3037b016c1f1
geethadevaiah <67411309+geethadevaiah@users.noreply.github.com>
2021-08-27 23:05:55 +0530

    Update NQueen_bitset.java

--- Class_revision/Recursion_and_Backtracking/NQueen_bitset.java
+++ Class_revision/Recursion_and_Backtracking/NQueen_bitset.java
@@ -1,6 +1,7 @@
 package Class_revision.Recursion_and_Backtracking;
 
 import java.util.BitSet;
+import java.util.List;
 
 /**
  * Find the number of ways Queens can be placed
@@ -16,15 +17,18 @@ public class NQueen_bitset {
 	static BitSet col = new BitSet(100), ld = new BitSet(100), rd = new BitSet(100);	
 	static int ans = 0;
 	
+	// for display purpose 
+	static boolean[][] grid = new boolean[10][10];
 	/**
 	 * @param n
 	 * @param cr - current row
 	 */
-	public static int nQueenCountUsingBitset(int n, int cr) {
+	public static int nQueenCountUsingBitset(int n, int cr, boolean[][] grid) {
 		// base case
 		if(n == cr) {
 			ans++;
-			//printQueensboard(n);
+			printQueensboard(n, grid);
+			System.out.println();
 			return ans;
 		}
 		// else check whether the bit sets are set in the current row and col
@@ -42,20 +46,33 @@ public class NQueen_bitset {
 				col.set(c, true);
 				ld.set(cr-c+n-1, true);
 				rd.set(cr+c, true);
-				nQueenCountUsingBitset(n, cr+1);
+				grid[cr][c] = true;
+				nQueenCountUsingBitset(n, cr+1, grid);
 				col.set(c, false);
 				ld.set(cr-c+n-1, false);
 				rd.set(cr+c, false);
+				grid[cr][c] = false;
 			}
 			
 		}
 		return ans;
 	}
 	
+	public static void printQueensboard(int n , boolean[][] grid) {
+		for(int i = 0 ; i < n ; i++) {
+			for(int j = 0 ; j < n ; j++) {
+				if(!grid[i][j]) System.out.print(" . ");
+				else
+					System.out.print(" Q ");
+			}
+			System.out.println();
+		}
+		
+	}
 	
 	public static void main(String[] args) {
 		int n = 5;
 		//col.set(0, 4); // sets the bits to true
-		System.out.println(nQueenCountUsingBitset(n, 0));
+		System.out.println(nQueenCountUsingBitset(n, 0, grid));
 	}
 }
